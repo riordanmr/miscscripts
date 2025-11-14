dd31ceaef5b7cdec5b8f56d28fce992cc0f4ef1d
Mark Riordan <riordan@rocketmail.com>
2025-11-14 10:45:53 -0600

    azurespeech.py: output to MP3 file.
    and tweak comments.

--- azurespeech.py
+++ azurespeech.py
@@ -1,5 +1,6 @@
-# Azure Speech Service Text to Speech Sample
-# based on https://ai.azure.com/resource/playground/speech/texttospeech?wsid=/subscriptions/02e0ac63-28ae-47cc-8aaa-1b447b7a80d3/resourceGroups/mrrp-aifoundry-rg/providers/Microsoft.CognitiveServices/accounts/mrrp-aifoundry/projects/mrrp-proj1&tid=63ce651b-d1c9-4ea5-8980-33a04e2157f0#voicegallery
+# Read a text file and produce an MP3 audio file containing the spoken text.
+# Based on this Azure Speech Service Text to Speech sample:
+# https://ai.azure.com/resource/playground/speech/texttospeech?wsid=/subscriptions/02e0ac63-28ae-47cc-8aaa-1b447b7a80d3/resourceGroups/mrrp-aifoundry-rg/providers/Microsoft.CognitiveServices/accounts/mrrp-aifoundry/projects/mrrp-proj1&tid=63ce651b-d1c9-4ea5-8980-33a04e2157f0#voicegallery
 # Mark Riordan  2025-11-10
 '''
   For more samples please visit https://github.com/Azure-Samples/cognitive-services-speech-sdk
@@ -8,6 +9,7 @@
 import azure.cognitiveservices.speech as speechsdk
 import sys
 import argparse
+import os
 
 class Settings:
     def __init__(self, input_file, speech_key, voice_name):
@@ -48,16 +50,25 @@ speech_config = speechsdk.SpeechConfig(subscription=settings.speech_key, region=
 # Note: the voice setting will not overwrite the voice element in input SSML.
 speech_config.speech_synthesis_voice_name = settings.voice_name
 
+# Set output format to MP3
+speech_config.set_speech_synthesis_output_format(
+    speechsdk.SpeechSynthesisOutputFormat.Audio16Khz32KBitRateMonoMp3
+)
+
 with open(settings.input_file, 'r') as f:
     text = f.read()
 
-# use the default speaker as audio output.
-speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+# Generate output filename: change extension to .mp3
+output_file = os.path.splitext(settings.input_file)[0] + '.mp3'
+
+# Configure audio output to file
+audio_config = speechsdk.audio.AudioOutputConfig(filename=output_file)
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
 result = speech_synthesizer.speak_text_async(text).get()
 # Check result
 if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-    print("Speech synthesized for text [{}]".format(text))
+    print("Speech synthesized to file: {}".format(output_file))
 elif result.reason == speechsdk.ResultReason.Canceled:
     cancellation_details = result.cancellation_details
     print("Speech synthesis canceled: {}".format(cancellation_details.reason))
